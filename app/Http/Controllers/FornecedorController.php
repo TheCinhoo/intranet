47c09b807a22b9555dbab901063561d087e76585
Fabricio V <thecinhoo@gmail.comm>
2023-04-10 17:42:57 -0300

    Começo do projeto de verdade

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,8 +9,35 @@ class FornecedorController extends Controller
     public function index()
     {
         $fornecedores = [
-            0 => ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj' => '00.000.000/0000-00']
+            0 => [
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '0',
+                'ddd' => '11', //São Paulo
+                'telefone' => '0000-0000'
+            ],
+            1 => [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+                'cnpj' => '12.345.678/0002-00',
+                'ddd' => '85', //Fortaleza
+                'telefone' => '0000-0000'
+            ],
+            2 => [
+                'nome' => 'Fornecedor 3',
+                'status' => 'S',
+                'cnpj' => '12.345.678/0001-00',
+                'ddd' => '32', //Juiz de Fora
+                'telefone' => '0000-0000'
+            ]
         ];
+
+        // Ternario
+        // condicao ? se verddadeiro : se falso;
+
+        // $msg = isset($fornecedores[0]['cnpj']) ? 'CNPJ Informado' : 'CNPJ não informado';
+        // echo $msg;
+
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -27,16 +27,48 @@ Para comentários de multiplas linhas
 
 Fornecedor: {{$fornecedores[0]['nome']}} <br>
 Status: {{$fornecedores[0]['status']}} <br>
-
+<hr>
 @if ( !($fornecedores[0]['status'] == 'S') )
-    Fornecedor Inativo
+    Fornecedor Inativo com If
 @endif
-
+<hr>
 @unless($fornecedores[0]['status'] == 'S') {{--Executa se o retorno da condição for false--}}
-    <br>Fornecedor Inativo
+    <br>Fornecedor Inativo Com unless
 @endunless
 <br>
 
+<hr>
 @isset($fornecedores)
-    {{-- Se a Variavel existir, então entramos nesse bloco podendo ser usado para verificar informações dentro de um array --}}
+    @forelse($fornecedores as $i => $fornecedor)
+
+        {{$loop->iteration}}
+        <br>
+
+        Fornecedor: {{$fornecedor['nome']}}
+        <br>
+        Status: {{$fornecedor['status']}}
+        <br>
+        CNPJ: {{$fornecedor['cnpj'] ?? 'dado não preenchido'}}
+        <br>
+        Telefone: ({{$fornecedor['ddd'] ?? ''}}) {{$fornecedor['telefone'] ?? ''}}
+        @switch($fornecedor['ddd'])
+            @case('11')
+                São Paulo - SP
+                @break
+            @case('85')
+                Fortaleza - CE
+                @break
+            @case('32')
+                Juiz de Fora - MG
+                @break
+            @default
+                Estado não identificado.
+        @endswitch
+        <hr>
+
+        @empty
+            Não existem fornecedores cadastrados.
+    @endforelse
 @endisset
+
+
