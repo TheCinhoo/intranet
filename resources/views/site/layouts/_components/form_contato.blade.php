b3e90039dd452aa80da32c1bd21044e7f8dd4d80
Fabricio V <thecinhoo@gmail.comm>
2023-04-11 17:37:18 -0300

    configurando banco, migration e formularios

---
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -0,0 +1,21 @@
+{{$slot}}
+
+<form action="{{ route('site.contato')}}" method="POST">
+    @csrf
+    <input type="text" name="nome" placeholder="Nome" class="{{$classe}}">
+    <br>
+    <input type="text" name="telefone" placeholder="Telefone" class="{{$classe}}">
+    <br>
+    <input type="text" name="email" placeholder="E-mail" class="{{$classe}}">
+    <br>
+    <select name="motivo_contato" class="{{$classe}}">
+        <option value="">Qual o motivo do contato?</option>
+        <option value="1">Dúvida</option>
+        <option value="2">Elogio</option>
+        <option value="3">Reclamação</option>
+    </select>
+    <br>
+    <textarea name="mensagem" class="{{$classe}}" placeholder="Preencha aqui a sua mensagem"></textarea>
+    <br>
+    <button type="submit" class="{{$classe}}">ENVIAR</button>
+</form>
---
+++ resources/views/site/layouts/basico.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>CASACARESC - @yield('titulo')</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href=" {{asset('css/estilo_basico.css')}}">
+</head>
+
+<body>
+    @include('site.layouts._partials.topo')
+    @yield('conteudo')
+    @include('site.layouts._partials.rodape')
+</body>
+
+</html>
